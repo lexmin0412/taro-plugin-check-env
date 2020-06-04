e43842d0036bb9bab2e2f58fcfdbda5e2dfa996a
张乐 <zhangle@hydee.com>
2020-06-04 20:19:35 +0800

    perf: 美化插件打印，去除无用代码

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = (ctx, options) => {
-    console.log('enter taro-plugin-check-env, options:', options);
-    // plugin 主体
+    console.log(ctx.helper.chalk.yellow('插件 '), 'taro-plugin-check-env');
+    console.log(ctx.helper.chalk.greenBright('开始 '), '检查环境变量');
+    const { ENV_LIST } = options;
+    const { initialConfig: { defineConstants: { APP_CONF } }, helper: { chalk } } = ctx;
     ctx.onBuildStart(() => {
-        console.log('编译开始！');
-    });
-    ctx.onBuildFinish(() => {
-        console.log('编译结束！');
+        for (const key in ENV_LIST) {
+            if (ENV_LIST.hasOwnProperty(key)) {
+                const element = ENV_LIST[key];
+                if (!APP_CONF[key]) {
+                    console.log(chalk.red('错误 '), `缺少环境变量: ${element} - ${key}`);
+                    throw '环境变量检查不通过';
+                }
+                else {
+                    console.log(`${chalk.magentaBright('读取 ')}`, `${key} `, APP_CONF[key]);
+                }
+            }
+        }
+        console.log(chalk.blueBright('结束 '), '环境变量检查通过✅');
+        console.log('');
     });
 };
 //# sourceMappingURL=index.js.map
