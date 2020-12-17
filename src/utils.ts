dce6aaf821440eae37162d6fbb583dcdc1afba95
lexmin0412 <zhangle_media@hotmail.com>
2020-12-18 06:30:21 +0800

    refactor: 用户未传入taroVersion时不做检测

--- src/utils.ts
+++ src/utils.ts
@@ -4,13 +4,20 @@
  * @param taroVersion taro版本号
  */
 export const checkTaroVersion = (platform: 'weapp' | 'h5', taroVersion: string, options: any) => {
-  const h5Version = options && options.taroVersion && options.taroVersion.h5 ? options.taroVersion.h5 : '2.2.7'
-  const weappVersion = options && options.taroVersion && options.taroVersion.h5 ? options.taroVersion.weapp : '2.2.11'
-  const platformText = platform === 'h5' ? 'h5' : '小程序';
-  const targetVersion = platform === 'h5' ? h5Version : weappVersion;
+  if (options && options.taroVersion) {
+    const h5Version = options && options.taroVersion && options.taroVersion.h5 ? options.taroVersion.h5 : '2.2.7'
+    const weappVersion = options && options.taroVersion && options.taroVersion.h5 ? options.taroVersion.weapp : '2.2.11'
+    const platformText = platform === 'h5' ? 'h5' : '小程序';
+    const targetVersion = platform === 'h5' ? h5Version : weappVersion;
+    return {
+      success: taroVersion === targetVersion,
+      platform,
+      errMsg: `当前平台为${platformText}, 需要安装对应的taro版本为${targetVersion}, 请检查当前项目依赖版本后重试`
+    }
+  }
   return {
-    success: taroVersion === targetVersion,
+    success: true,
     platform,
-    errMsg: `当前平台为${platformText}, 需要安装对应的taro版本为${targetVersion}, 请检查当前项目依赖版本后重试`
+    errMsg: `用户未指定Taro版本，跳过检测`
   }
 } 
